974f5b73a3aed9cbb24a2aa39caa744dff9a5122
Reese Gyllenhammer <reesegyllenhammer@Reeses-MacBook-Pro-2.local>
2022-12-22 15:53:21 -0800

    added dotfiles and connection to firebase

--- index.js
+++ index.js
@@ -2,6 +2,28 @@
 const express = require('express');
 const app = express();
 const path = require("path");
+const firebase = require("firebase/app");
+const firebaseDB = require("firebase/database");
+require('dotenv').config()
+
+// set up and initialize firebase
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  databaseURL: process.env.FIREBASE_DATABASE_URL
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const database = firebaseDB.getDatabase(firebaseApp);
+
+// set a fun new thing
+firebaseDB.set(firebaseDB.ref(database, 'test/'), {
+    username: 'hello reese',
+});
 
 // home view
 app.get('/', (req, res) => {
