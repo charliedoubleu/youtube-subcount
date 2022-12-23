1ff0660f0a9838c532c4a7367a91b8f299819f43
Reese Gyllenhammer <reesegyllenhammer@Reeses-MacBook-Pro-2.local>
2022-12-23 01:12:34 -0800

    created explicit route for particles

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 const path = require("path");
-const firebase = require("firebase/app");
-const firebaseDB = require("firebase/database");
 const mustacheExpress = require('mustache-express');
 
 // imports written by us
-const firebaseHelper = require(path.join(__dirname, 'helpers/FirebaseHelper'));
+const particlesRoute = require(path.join(__dirname, 'routes/particles'));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'mustache');
@@ -17,29 +15,12 @@ app.use('/static', express.static(path.join(__dirname, 'static')))
 app.use(express.json());
 app.engine('mustache', mustacheExpress());
 
+// path for particles related code
+app.use('/particles', particlesRoute);
+
 // home view
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "static/html/index.html"));
-});
-
-app.get('/particles', async (req, res) => {
-    // TODO: get this number properly from youtube api based on a key
-    const subscriberCount = 12;
-    const numberParticlesInDB = await firebaseHelper.getTotalParticleCount();
-    const newParticlesToMake = subscriberCount - numberParticlesInDB;
-
-    if (newParticlesToMake > 0){
-        firebaseHelper.createUnclaimedParticles(newParticlesToMake, subscriberCount);
-    }
-
-    const particles = await firebaseHelper.getAllParticles();
-    res.render('particles', {particles: particles});
-});
-
-app.post('/particles/claim-particle', (req, res) => {
-    const { firebaseKey } = req.body;
-    console.log(firebaseKey);
-    return res.status(200).send({status: 'claimed'});
 });
 
 app.listen(
---
+++ routes/particles.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const express = require('express');
+const router = express.Router();
+const firebaseHelper = require(path.join(__dirname, '../helpers/FirebaseHelper'));
+
+router.get('/', async (req, res) => {
+    // TODO: get this number properly from youtube api based on a key
+    const subscriberCount = 12;
+    const numberParticlesInDB = await firebaseHelper.getTotalParticleCount();
+    const newParticlesToMake = subscriberCount - numberParticlesInDB;
+
+    if (newParticlesToMake > 0){
+        firebaseHelper.createUnclaimedParticles(newParticlesToMake, subscriberCount);
+    }
+
+    const particles = await firebaseHelper.getAllParticles();
+    res.render('particles', {particles: particles});
+});
+
+router.post('/claim-particle', (req, res) => {
+    const { firebaseKey } = req.body;
+    console.log(firebaseKey);
+    return res.status(200).send({status: 'claimed'});
+});
+
+module.exports = router
