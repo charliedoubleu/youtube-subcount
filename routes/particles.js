8f2d6192de935b0e1d5fc47dadd90451b30467d6
Reese Gyllenhammer <reesegyllenhammer@Reeses-MacBook-Pro-2.local>
2022-12-24 14:42:43 -0800

    shortened import names

--- routes/particles.js
+++ routes/particles.js
@@ -1,60 +1,44 @@
 const path = require("path");
 const express = require('express');
 const router = express.Router();
-const firebaseHelper = require(path.join(__dirname, '../helpers/FirebaseHelper'));
-const youtubeHelper = require(path.join(__dirname, '../helpers/YoutubeHelper'));
-
-router.get('/', async (req, res) => {
-    const subscriberCount = await youtubeHelper.getYouTubeSubCount(
-        process.env.YOUTUBE_API_KEY_REESE,
-        process.env.YOUTUBE_USER_REESE
-    );
-    const numberParticlesInDB = await firebaseHelper.getTotalParticleCount();
-    const newParticlesToMake = subscriberCount - numberParticlesInDB;
-
-    if (newParticlesToMake > 0){
-        firebaseHelper.createUnclaimedParticles(newParticlesToMake, subscriberCount);
-    }
-
-    const particles = await firebaseHelper.getAllParticles();
-    res.render('particles', {particles: particles});
-});
+const fbh = require(path.join(__dirname, '../helpers/FirebaseHelper'));
+const yth = require(path.join(__dirname, '../helpers/YoutubeHelper'));
 
 router.get('/reese', async (req, res) => {
-    const subscriberCount = await youtubeHelper.getYouTubeSubCount(
+    const subscriberCount = await yth.getYouTubeSubCount(
         process.env.YOUTUBE_API_KEY_REESE,
         process.env.YOUTUBE_USER_REESE
     );
-    const numberParticlesInDB = await firebaseHelper.getTotalParticleCount();
+    const numberParticlesInDB = await fbh.getTotalParticleCount();
     const newParticlesToMake = subscriberCount - numberParticlesInDB;
 
     if (newParticlesToMake > 0){
-        firebaseHelper.createUnclaimedParticles(newParticlesToMake, subscriberCount);
+        fbh.createUnclaimedParticles(newParticlesToMake, subscriberCount);
     }
 
-    const particles = await firebaseHelper.getAllParticles();
+    const particles = await fbh.getAllParticles();
     res.render('index', {particles: particles});
 });
 
 router.get('/charlie', async (req, res) => {
-    const subscriberCount = await youtubeHelper.getYouTubeSubCount(
+    const subscriberCount = await yth.getYouTubeSubCount(
         process.env.YOUTUBE_API_KEY_CHARLIE,
         process.env.YOUTUBE_USER_CHARLIE
     );
-    const numberParticlesInDB = await firebaseHelper.getTotalParticleCount();
+    const numberParticlesInDB = await fbh.getTotalParticleCount();
     const newParticlesToMake = subscriberCount - numberParticlesInDB;
 
     if (newParticlesToMake > 0){
-        firebaseHelper.createUnclaimedParticles(newParticlesToMake, subscriberCount);
+        fbh.createUnclaimedParticles(newParticlesToMake, subscriberCount);
     }
 
-    const particles = await firebaseHelper.getAllParticles();
+    const particles = await fbh.getAllParticles();
     res.render('index', {particles: particles});
 });
 
 router.post('/claim-particle', (req, res) => {
     const { firebaseKey, accessKey, nickname } = req.body;
-    firebaseHelper.claimParticle(firebaseKey, accessKey, nickname);
+    fbh.claimParticle(firebaseKey, accessKey, nickname);
     return res.status(200).send({status: 'claimed'});
 });
 
